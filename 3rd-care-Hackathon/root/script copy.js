559ec4c3e9e38683bfb9104b5e82c464ff157e40
OfqfFw2bybPDYsT8fPt4 <catn1200078@gn.iwasaki.ac.jp>
2021-06-21 08:32:12 +0900

    a

---
+++ 3rd-care-Hackathon/root/script copy.js
@@ -0,0 +1,273 @@
+// Show or hide password
+$('#pw-toggle').on('click',function(){
+
+  let state = $('#password').prop('checked');
+
+  if(!state){
+    $('#password').prop('checked',true);
+    $('#password').prop('type','text');
+  }
+  else{
+    $('#password').prop('checked',false);
+    $('#password').prop('type','password');
+  }
+  
+})
+
+// Foolproof on user registration page
+document.getElementById('change').on('click',function(){
+  const fee = confirm('この内容で登録します。よろしいですか？');
+  if(fee){
+    location.href = 'login.html';
+  }
+  else {
+    location.href = '#';
+  };
+});
+
+// Change and save password after login
+$('#change').on('click',function(){
+
+  let state = $('.form-control').prop('disabled');
+
+  if(state){
+    $('.form-control').prop('disabled',false);
+    $('#pw-toggle').prop('disabled',false);
+    $('#change').attr('value','保存');
+  }
+  else{
+
+    let ans = confirm('この内容に変更します。よろしいですか？');
+
+    if(ans){
+      $('#password').prop('type','password');
+      $('.form-control').prop('disabled',true);
+      $('#pw-toggle').prop('checked',false);
+      $('#pw-toggle').prop('disabled',true);
+      $('#change').attr('value','変更');
+    }
+    else{
+      location.href = '#';
+    }
+    
+  }
+
+})
+
+// Validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+// Use of open data sources
+// List of regional disaster prevention bases in Yokohama
+
+// マップ生成のための callback 関数
+function initMap() {
+
+  // マップ生成のためのテストデータ
+  let markerData = 
+  [
+    {
+      lat: 35.495675,
+      lng: 139.67078,
+      content:
+      '<table class="table">'+
+        '<tr>'+
+          '<th scope="row">混雑度</th>'+
+          '<td>'+
+            '<div class="progress">'+
+              '<div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>'+
+            '</div>'+
+          '</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">名前</th>'+
+          '<td>生麦小学校</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">住所</th>'+
+          '<td>生麦四丁目15番1号</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">備考</th>'+
+          '<td>'+'被災した住民の避難生活の場所、情報受伝達、備蓄機能を備えた拠点です。'+'</td>'+
+        '</tr>'+
+      '</table>'+
+      '<a href="shelter.html" class="btn btn-primary">避難所の詳細</a>',
+      icon: ''
+    },
+    {
+      lat: 35.4961022924626,
+      lng: 139.6695676445758,
+      content:
+        '<div class="card" class="img-fluid">'+
+          '<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">'+
+            '<ol class="carousel-indicators">'+
+              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>'+
+              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>'+
+              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>'+
+            '</ol>'+
+            '<div class="carousel-inner">'+
+              '<div class="carousel-item active" data-bs-interval="∞">'+
+                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
+              '</div>'+
+              '<div class="carousel-item">'+
+                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
+              '</div>'+
+              '<div class="carousel-item">'+
+                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
+              '</div>'+
+            '</div>'+
+            '<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">'+
+              '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
+              '<span class="visually-hidden">Previous</span>'+
+            '</a>'+
+            '<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">'+
+              '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
+              '<span class="visually-hidden">Next</span>'+
+            '</a>'+
+          '</div>'+
+          '<div class="card-body">'+
+            '<h5 class="card-title">タイトル</h5>'+
+            '<p class="card-text">投稿者コメント</p>'+
+          '</div>'+
+        '</div>'+
+        '<br>'+
+        '<a href="danger.html" class="btn btn-primary">＋ 追加する</a>'
+      ,
+      icon: 'data/skull.png'
+    },
+    {
+      lat: 35.4953,
+      lng: 139.66695,
+      content:
+      '<table class="table">'+
+        '<tr>'+
+          '<th scope="row">混雑度</th>'+
+          '<td>0%</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">名前</th>'+
+          '<td>生麦小学校</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">住所</th>'+
+          '<td>生麦四丁目15番1号</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<th scope="row">備考</th>'+
+          '<td>'+'被災した住民の避難生活の場所、情報受伝達、備蓄機能を備えた拠点です。'+'</td>'+
+        '</tr>'+
+      '</table>'+
+      '<a href="shelter.html" class="btn btn-primary">避難所の詳細</a>',
+      icon: 'data/male-2.png'
+    }
+  ];
+
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+
+  let latlng = new google.maps.LatLng({
+    lat: markerData[0]['lat'],
+    lng: markerData[0]['lng']
+  });
+
+  // 初期マップ生成のためのパラメタ
+  var opts = {
+    zoom: 16,
+    center: latlng,
+    streetViewControl: true,
+    mapTypeControl: true
+  };
+
+  // 初期マップの生成
+  let map = new google.maps.Map(document.getElementById("map"), opts);
+
+  directionsRenderer.setMap(map);
+
+  // 初期マーカーの生成
+  for(var i=0; i<markerData.length; i++){
+
+    let marker = new google.maps.Marker({
+      position: {
+        lat: markerData[i]['lat'],
+        lng: markerData[i]['lng']  
+      },
+      map: map
+    });
+
+  };
+
+  var infowindow = new google.maps.InfoWindow({
+    position: {
+      lat: markerData[1]['lat'],
+      lng: markerData[1]['lng']
+    },
+    content: markerData[1]['content']
+  });
+
+  var marker = new google.maps.Marker({
+    position: {
+      lat: markerData[1]['lat'],
+      lng: markerData[1]['lng'],
+    },
+    map: map
+  });
+
+  // マーカーをタップ時に infowindow を展開
+  google.maps.event.addListener(marker,'click',function(){
+    infowindow.open(map, marker);
+  });  
+
+  // 画面タップ時にマーカーを生成
+  google.maps.event.addListener(map, 'click', event => clickListener(event, map));
+
+  // ルート案内
+  calcRoute(directionsService, directionsRenderer);
+};
+
+// ルート案内のための関数を定義
+function calcRoute(directionsService, directionsRenderer){
+  directionsService.route(
+    {
+      origin: {
+        lat: markerData[0]['lat'],
+        lng: markerData[0]['lng']
+      },
+      destination: {
+        lat: markerData[1]['lat'],
+        lng: markerData[1]['lng']
+      },
+      travelMode: google.maps.travelMode.WALKING,  
+    }
+  );
+  (response, status) => {
+    if (status == "OK") {
+      directionsRenderer.setDirections(response);
+    } else {
+      window.alert("Directions request failed due to " + status);
+    };
+  };
+};
+
+
+// support-team/users
+// Sorting
