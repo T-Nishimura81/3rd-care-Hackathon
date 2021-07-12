40417ff0a7c49562acc6d0edc32029ce0f57de1a
OfqfFw2bybPDYsT8fPt4 <catn1200078@gn.iwasaki.ac.jp>
2021-07-12 13:56:31 +0900

    re:mainへのmergeのためのファイル整理

--- root/scripts/map.js
+++ root/scripts/map.js
@@ -13,102 +13,6 @@
 */
 
 async function initMap() {
-
-  // マップ生成のためのテストデータ
-  const markerData = 
-  [
-    {
-      lat: 35.495675,
-      lng: 139.67078,
-      content:
-      '<table class="table">'+
-        '<tr>'+
-          '<th scope="row">混雑度</th>'+
-          '<td>'+
-            '<div class="progress">'+
-              '<div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>'+
-            '</div>'+
-          '</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">名前</th>'+
-          '<td>生麦小学校</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">住所</th>'+
-          '<td>生麦四丁目15番1号</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">備考</th>'+
-          '<td>'+'被災した住民の避難生活の場所、情報受伝達、備蓄機能を備えた拠点です。'+'</td>'+
-        '</tr>'+
-      '</table>'+
-      '<a href="shelter.html" class="btn btn-primary">避難所の詳細</a>',
-    },
-    {
-      lat: 35.4961022924626,
-      lng: 139.6695676445758,
-      content:
-        '<div class="card" class="img-fluid">'+
-          '<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">'+
-            '<ol class="carousel-indicators">'+
-              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>'+
-              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>'+
-              '<li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>'+
-            '</ol>'+
-            '<div class="carousel-inner">'+
-              '<div class="carousel-item active" data-bs-interval="∞">'+
-                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
-              '</div>'+
-              '<div class="carousel-item">'+
-                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
-              '</div>'+
-              '<div class="carousel-item">'+
-                '<img src="../data/blueBack.jpg" class="d-block w-100" alt="no image">'+
-              '</div>'+
-            '</div>'+
-            '<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">'+
-              '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-              '<span class="visually-hidden">Previous</span>'+
-            '</a>'+
-            '<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">'+
-              '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-              '<span class="visually-hidden">Next</span>'+
-            '</a>'+
-          '</div>'+
-          '<div class="card-body">'+
-            '<h5 class="card-title">タイトル</h5>'+
-            '<p class="card-text">投稿者コメント</p>'+
-          '</div>'+
-        '</div>'+
-        '<br>'+
-        '<a href="danger.html" class="btn btn-primary">＋ 追加する</a>'
-    },
-    {
-      lat: 35.4953,
-      lng: 139.66695,
-      content:
-      '<table class="table">'+
-        '<tr>'+
-          '<th scope="row">混雑度</th>'+
-          '<td>0%</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">名前</th>'+
-          '<td>生麦小学校</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">住所</th>'+
-          '<td>生麦四丁目15番1号</td>'+
-        '</tr>'+
-        '<tr>'+
-          '<th scope="row">備考</th>'+
-          '<td>'+'被災した住民の避難生活の場所、情報受伝達、備蓄機能を備えた拠点です。'+'</td>'+
-        '</tr>'+
-      '</table>'+
-      '<a href="shelter.html" class="btn btn-primary">避難所の詳細</a>',
-    },
-  ];
   
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
@@ -125,30 +29,26 @@ async function initMap() {
     },
   });
 
+  const target = document.getElementById("map");
+  const observer = new MutationObserver(function (mutations, observer){
+    
+  });
+  const config = {
+    childList: true,
+    subtree: true
+  };
+
+  observer.observe(target, config);
+
   // 現在地にマーカーを立てるUIの追加
-  addUI(map)
+  addUI(map, directionsService, directionsRenderer)
 
   // 現在地からルート案内を行う
 
 }
 
-// 関数マネージャ
-function functionManager(event, map, directionsService, directionsRenderer) {
-  let lat = event.latLng.lat();
-  console.log(lat)
-  let lng = event.latLng.lng();
-  console.log(lng)
-  let marker = new google.maps.Marker({
-    position: {lat, lng},
-  });
-  
-  // ルート案内
-  directionsRenderer.setMap(map);
-  calculateAndDisplayRoute(directionsService, directionsRenderer, lat, lng)
-}
-
 // カスタムUIの追加
-function addUI(map) {
+function addUI(map, directionsService, directionsRenderer) {
   const UIbg = document.createElement('div');
   const UI = document.createElement('img');
 
@@ -163,25 +63,25 @@ function addUI(map) {
   map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(UIbg);
 
   UIbg.addEventListener("click", () => {
-    geolocation(map)
-  });
+    geolocation(map, directionsService, directionsRenderer)
+  }, false);
 }
 
 // 現在地の取得
-function geolocation(map) {
+function geolocation(map, directionsService, directionsRenderer) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         let lat = position.coords.latitude;
         let lng = position.coords.longitude;
-        markerGenerate(map, lat, lng)
+        markerGenerate(map, lat, lng, directionsService, directionsRenderer)
       }
     );
   }
 }
 
 // マーカーを生成する関数
-function markerGenerate(map, lat, lng) {
+function markerGenerate(map, lat, lng, directionsService, directionsRenderer) {
 
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
@@ -205,13 +105,17 @@ function markerGenerate(map, lat, lng) {
   })
   marker.setMap(map);
 
+  // googleMapのcenterを変更する
+  map.setCenter(new google.maps.LatLng(lat, lng));
+
+  // googleMapのcenterを変更するとUIが消えるので再追加する
   addUI(map)
-  infowindowGenerate(marker, lat, lng)
-  
+
+  infowindowGenerate(marker, lat, lng, directionsService, directionsRenderer)
 };
 
 // 情報ウィンドウを生成する関数
-function infowindowGenerate(marker, lat, lng) {
+function infowindowGenerate(marker, lat, lng, directionsService, directionsRenderer) {
 
   let infowindow = new google.maps.InfoWindow({
     position: {
@@ -219,13 +123,14 @@ function infowindowGenerate(marker, lat, lng) {
       lng: lng
     },
     content:
-    '<a href="../use-base-material-kit/danger.html" class="btn btn-primary">危険地点を共有する</a>'+
-    '<br>'+
-    '<button id="pre_loc" class="btn btn-primary">ここから避難所まで行く</button>'
+    '<div id="pre_loc_div">'+
+      '<a href="../use-base-material-kit/danger.html" class="btn btn-primary">危険地点を共有する</a>'+
+      '<br>'+
+      '<button id="pre_loc" class="btn btn-primary">ここから避難所まで行く</button>'+
+    '</div>'
   });
   infowindow.open(map, marker);
 
-  Directions(directionsService, directionsRenderer, lat, lng)
 }
 
 // ルート案内する関数
